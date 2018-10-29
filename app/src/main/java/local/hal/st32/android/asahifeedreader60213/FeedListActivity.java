3d264bc95ddf08f6cae570a10870846b09184d37
tnkTaka <taka.abc.hiko@gmail.com>
2018-10-29 11:49:53 +0900

    Initial commit

---
+++ app/src/main/java/local/hal/st32/android/asahifeedreader60213/FeedListActivity.java
@@ -0,0 +1,13 @@
+package local.hal.st32.android.asahifeedreader60213;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FeedListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed_list);
+    }
+}
